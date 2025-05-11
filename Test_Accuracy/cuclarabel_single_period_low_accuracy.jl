b2e02e64677f154baa3527c725d2d6356697b748
Franc-Z <85468633+Franc-Z@users.noreply.github.com>
2025-05-12 01:04:41 +0800

    Update cuclarabel_single_period_low_accuracy.jl

--- Test_Accuracy/cuclarabel_single_period_low_accuracy.jl
+++ Test_Accuracy/cuclarabel_single_period_low_accuracy.jl
@@ -26,12 +26,9 @@ set_optimizer_attribute(model, "dynamic_regularization_enable", false)
 set_optimizer_attribute(model, "chordal_decomposition_enable", false)
 set_optimizer_attribute(model, "equilibrate_enable", false)
 set_optimizer_attribute(model, "verbose", true)
-set_optimizer_attribute(model, "tol_gap_abs", 1e-4)
-set_optimizer_attribute(model, "tol_gap_rel", 1e-4)
-set_optimizer_attribute(model, "tol_feas", 1e-4)
-#set_optimizer_attribute(model, "tol_infeas_abs", 5e-12)
-#set_optimizer_attribute(model, "tol_infeas_rel", 5e-5)
-set_optimizer_attribute(model, "tol_ktratio", 1e-4)
+set_optimizer_attribute(model, "tol_gap_abs", 1e-8)
+set_optimizer_attribute(model, "tol_gap_rel", 1e-8)
+set_optimizer_attribute(model, "tol_feas", 1e-8)
 
 # 定义变量
 @variables(model, begin
@@ -60,7 +57,6 @@ optimize!(model)
 
 # 获取底层求解器
 my_solver = model.moi_backend.optimizer.model.optimizer.solver
-
 # 输出结果
 CUDA.@allowscalar begin
     local x_opt = vec(my_solver.solution.x[1:n])
@@ -70,6 +66,7 @@ CUDA.@allowscalar begin
     end
     println("")
 end
+
 
 # 优化数据拷贝操作
 new_q = CUDA.similar(my_solver.data.q, Float64)
@@ -77,24 +74,60 @@ new_b = CUDA.similar(my_solver.data.b, Float64)
 CUDA.copyto!(new_q, my_solver.data.q)
 CUDA.copyto!(new_b, my_solver.data.b)
 
-# 在此部分我希望给mu_matrix的原有值加上其(-3% ~ 3%)之内的随机扰动
-random_noise = 0.06 * randn(size(mu_matrix))
-mu_matrix .*= (1.0 .+ random_noise)
-
 # 重复求解并计时
 CUDA.@time begin
-    CUDA.copyto!(new_q[1:n], -mu_matrix)
+    #=
+    target_value = -mu_matrix[n]  # 替换为您想比较的值
+    # 将 CUDA 稀疏矩阵转换到 CPU 后再进行查找
+    cpu_q = Array(my_solver.data.q)
+    indices = findall(x -> isapprox(x, target_value), cpu_q)
+    println("Indices of elements approximately equal to $target_value: ", indices)
+    =#
+    # 在此部分我希望给mu_matrix的原有值加上其(-3% ~ 3%)之内的随机扰动
+    println(mu_matrix[1:5])
+    random_noise = 0.1 .* randn(size(mu_matrix))
+    mu_matrix .*= (1.0 .+ random_noise)
+    println(mu_matrix[1:5])
+    # 将新的目标函数系数和约束右侧值传递给求解器
+    CUDA.copyto!(CUDA.view(new_q, 1:n), -mu_matrix[:])
+    println(new_q[1:5])
     Clarabel.update_q!(my_solver, new_q)
     Clarabel.update_b!(my_solver, new_b)
-    Clarabel.solve!(my_solver, true)
+    solution = Clarabel.solve!(my_solver, true)
 end
 
 # 输出结果
 CUDA.@allowscalar begin
-    local x_opt = vec(my_solver.solution.x[1:n])
+    local x_opt = vec(solution.x[1:n])
     local top10_idx = partialsortperm(x_opt, 1:10, rev = true)
     for (i, idx) in enumerate(top10_idx)
         @printf("排名 %2d: 资产 %4d, 权重 = %.6f\n", i, idx, x_opt[idx])
     end
     println("")
+end
+
+# 从第一个时间段获取最优解作为新的初始持仓量
+CUDA.@allowscalar for i in 1:1
+    #copyto!(x0, value.(x[:]))
+    #set_normalized_rhs.(con_1, -x0)
+    #set_normalized_rhs.(con_2, x0)
+    
+    set_objective_coefficient.(model, x[:], -mu_matrix[:])
+    
+    println("设置目标函数系数和初始值完成")
+    @time optimize!(model)
+end
+
+# 输出结果
+CUDA.@allowscalar begin
+    println("预期收益 = ", value(expected_return))
+    println("交易成本 = ", value(transaction_fee))
+
+    println("前10个最大权重及其指数:")
+    # 使用高效部分排序而非完全排序
+    local x_opt = vec(value.(x))
+    local top10_idx = partialsortperm(x_opt, 1:10, rev=true)
+    for (i, idx) in enumerate(top10_idx)
+        @printf("排名 %2d: 资产 %4d, 权重 = %.6f\n", i, idx, x_opt[idx])
+    end
 end
